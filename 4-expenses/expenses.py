fe2188d55b2d792b77b2eb5df29c89f5b13caf89
Uge <eugelikm@gmail.com>
2026-05-15 23:51:33 +0200

    7.11. Домашнее задание - Строки

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -5,17 +5,17 @@ is_correect_format: bool = True
 
 if len(temps_summ) != 2 and len(temps_summ) != 4:
     is_correect_format = False
-    print("Вы ввели сумму в неверном формате")
+    print("Некорректный формат суммы")
 else:
-    if len(temps_summ) == 2 and temps_summ[1] != "руб":
+    if len(temps_summ) == 2 and (temps_summ[1] != "руб" or temps_summ[0].isdigit() == False):
         is_correect_format = False
-        print("Вы ввели сумму в неверном формате")
-    elif len(temps_summ) == 4 and (temps_summ[1] != "руб" or temps_summ[3] != "коп"):
+        print("Некорректный формат суммы")
+    elif len(temps_summ) == 4 and (temps_summ[1] != "руб" or temps_summ[3] != "коп" or temps_summ[2].isdigit() == False):
         is_correect_format = False
-        print("Вы ввели сумму в неверном формате")
+        print("Некорректный формат суммы")
 
 if is_correect_format:
     if len(temps_summ) == 2:
         print(f"{temps_summ[0]}.00 ₽")
     else:
-        print(f"{temps_summ[0]}.{temps_summ[2]} ₽")
+        print(f"{temps_summ[0]}.{temps_summ[2].zfill(2)} ₽")
