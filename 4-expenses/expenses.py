9f579e903048e2abbab865e1ba59aba8da04aa50
Uge <eugelikm@gmail.com>
2026-05-23 20:55:47 +0200

    8.13. Домашнее задание - Циклы

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,22 +1,38 @@
-summ: str = str(input("Введите сумму:")).lower().strip()
+# summ: str = str(input("Введите сумму:")).lower().strip()
 
-temps_summ = summ.split()
-is_correect_format: bool = True
+# temps_summ = summ.split()
+# is_correect_format: bool = True
 
-if len(temps_summ) != 2 and len(temps_summ) != 4:
-    is_correect_format = False
-    print("Некорректный формат суммы")
-else:
-    if len(temps_summ) == 2 and (temps_summ[1] != "руб" or temps_summ[0].isdigit() == False):
-        is_correect_format = False
-        print("Некорректный формат суммы")
-    elif len(temps_summ) == 4 and (temps_summ[1] != "руб" or temps_summ[3] != "коп" or temps_summ[2].isdigit() == False):
-        is_correect_format = False
-        print("Некорректный формат суммы")
+# if len(temps_summ) != 2 and len(temps_summ) != 4:
+#     is_correect_format = False
+#     print("Некорректный формат суммы")
+# else:
+#     if len(temps_summ) == 2 and (temps_summ[1] != "руб" or temps_summ[0].isdigit() == False):
+#         is_correect_format = False
+#         print("Некорректный формат суммы")
+#     elif len(temps_summ) == 4 and (temps_summ[1] != "руб" or temps_summ[3] != "коп" or temps_summ[2].isdigit() == False):
+#         is_correect_format = False
+#         print("Некорректный формат суммы")
 
-if is_correect_format:
+# if is_correect_format:
 
-    if len(temps_summ) == 2:
-        print(f"{temps_summ[0]}.00 ₽")
+#     if len(temps_summ) == 2:
+#         print(f"{temps_summ[0]}.00 ₽")
+#     else:
+#         print(f"{temps_summ[0]}.{temps_summ[2].zfill(2)} ₽")
+
+expenses: list = []  # type: ignore
+while True:
+    print("1. Добавить расход")
+    print("2. Показать все расходы")
+    print("3. Показать сумму и средний расход")
+    print("4. Удалить расход по номеру")
+    print("5. Выход")
+
+    choice: str = input("Выберите действие: ")
+
+    if choice == "5":
+        print("Выход из программы.")
+        break
     else:
-        print(f"{temps_summ[0]}.{temps_summ[2].zfill(2)} ₽")
+        print("Некорректный выбор. Пожалуйста, выберите 1, 2 или 3.")
